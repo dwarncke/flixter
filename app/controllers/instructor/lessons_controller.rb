b411a18ca23c9e8aaa8b92fb566e9dd5b7f3d7a8
dwarncke <daniel.warncke@gmail.com>
2019-09-17 15:38:24 +0000

    sections update

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -9,16 +9,23 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def create
-    @lesson = current_section.lessons.create(lesson_params)
+    @lesson = current_section.lessons_path(section_params)
     redirect_to instructor_course_path(current_section.course)
   end
 
   def update
+    current_section.update_attributes(section_params)
     current_lesson.update_attributes(lesson_params)
     render plain: 'updated!'
   end
 
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
 
   def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
@@ -44,4 +51,7 @@ class Instructor::LessonsController < ApplicationController
   def lesson_params
      params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
+end
+end
+end
 end
